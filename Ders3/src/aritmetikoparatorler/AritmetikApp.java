0e965c0cfb1758996b4ca89003fa857df22ff8e2
ufuk.paksoy <ufuk.paksoy@icloud.com>
2019-02-02 10:34:34 +0300

    Aritmetik İşlemler

--- Ders3/src/aritmetikoparatorler/AritmetikApp.java
+++ Ders3/src/aritmetikoparatorler/AritmetikApp.java
@@ -2,5 +2,16 @@ package aritmetikoparatorler;
 
 public class AritmetikApp {
 
-    public static void main(String[])
+    public static void main(String[] args) {
+
+        system.out.println(Merhaba ilk branc)
+
+
+
+
+
+    }
+
+
+
 }
