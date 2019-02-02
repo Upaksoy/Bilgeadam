c721d7184bbf699539c5f8a11ea42cc14bfe0f17
ufuk.paksoy <ufuk.paksoy@icloud.com>
2019-02-02 12:40:22 +0300

    Aritmetik ve ilişkisel karşılaştırmalar

--- Ders2/src/İlkelTipler/Main.java
+++ Ders2/src/İlkelTipler/Main.java
@@ -1,32 +1,39 @@
-package İlkelTipler;
+package ilkeltipler;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-
-
-        Long a = 3000000000000000000L;
+        int a=5;
         System.out.println("Bu bir integer değer: "+a);
+        Integer b=5;
+        System.out.println("Bu bir integer değer: "+b);
+        Integer c=new Integer(5);
+        System.out.println("Bu bir integer değer: "+c);
 
+        int x;
+        x=12;
+        x=7;
+        System.out.println(x);
 
         long along=2l;
-        System.out.println("bu bir long değer: " +along);
+        System.out.println("Bu bir long değer: "+along);
         Long bLong=2L;
-        System.out.println("bu bir long değer: "+bLong);
-        Long cLong=new Long ('2');
-        System.out.println("bu bir long değer: "+cLong);
-
+        System.out.println("Bu bir long değer: "+bLong);
+        Long cLong=new Long(2);
+        System.out.println("Bu bir long değer: "+cLong);
 
         short aShort=50;
-        System.out.println("bu bir short değer: "+aShort);
-        Short cShort=50;
-        System.out.println("Bu bir short değer "+cShort);
+        System.out.println("Bu bir short değer: "+aShort);
+        Short bShort=50;
+        System.out.println("Bu bir short değer: "+bShort);
+        Short cShort=new Short((short) 50);
+        System.out.println("Bu bir short değer: "+cShort);
 
         byte aByte=20;
-        System.out.println("bu bir byte değer. "+aByte);
+        System.out.println("Bu bir byte değer: "+aByte);
         Byte bByte=20;
-        System.out.println("bu bir byte değer: "+bByte);
+        System.out.println("Bu bir byte değer: "+bByte);
         Byte cByte=new Byte((byte) 20);
         System.out.println("Bu bir byte değer: "+cByte);
 
@@ -34,14 +41,15 @@ public class Main {
         System.out.println("Bu bir char değer: "+achar);
         Character bChar='a';
         System.out.println("Bu bir char değer: "+bChar);
-        Character cChar=new Character ('a');
+        Character cChar=new Character('a');
         System.out.println("Bu bir char değer: "+cChar);
 
-        String  aString ="aboda";
+        String aString="abcde";
         System.out.println("Bu bir char değer: "+aString);
-        String bString=new String( "aboda");
-
+        String bString=new String("abcde");
+        System.out.println("Bu bir char değer: "+bString);
 
 
     }
 }
+
---
+++ Ders3/src/aritmetikoparatorler/İliskiselKarsilastirmaoparatörleri.java
@@ -0,0 +1,43 @@
+package aritmetikoparatorler;
+
+public class İliskiselKarsilastirmaoparatörleri {
+
+    public static void main(String[] args) {
+
+        int a=5,b=10;
+
+        // <, <=, >, >=, ==, !=
+
+
+        boolean sonuc;
+        sonuc=a<b;
+        System.out.println("A'nın B'den küçük olma durumu: " +sonuc);
+        System.out.println(a<b);
+
+        sonuc=a>b;
+        System.out.println("A'nın B'den büyük olma durumu: " +sonuc);
+
+        sonuc=a<=b;
+        System.out.println("A'nın B'den küçük eşit olma durumu: "+sonuc);
+
+        int c=5;
+        sonuc=a<=c;
+        System.out.println("A'nın C'den küçük eşit olma durumu: " +sonuc);
+
+        sonuc=a>=b;
+        System.out.println("A'nın B'den büyük eşit olma durumu:" +sonuc);
+
+        sonuc=a==b;
+        System.out.println("A'nın B'ye eşit olma durumu: " +sonuc);
+
+        sonuc=a!=b;
+        System.out.println("A'nın B'ye eşit olmama durumu: "+sonuc);
+
+
+
+
+
+
+    }
+
+}
