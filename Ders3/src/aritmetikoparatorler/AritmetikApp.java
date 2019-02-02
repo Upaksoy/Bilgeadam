2144b7fc5f784b6649032dbea6dc69851e2f8517
ufuk.paksoy <ufuk.paksoy@icloud.com>
2019-02-02 12:43:50 +0300

    Aritmetik İşlemler

--- Ders3/src/aritmetikoparatorler/AritmetikApp.java
+++ Ders3/src/aritmetikoparatorler/AritmetikApp.java
@@ -12,9 +12,68 @@ public class AritmetikApp {
 
         System.out.println("Merhaba ilk branch");
 
+         /*
+         operants(oparatörler)         karsılastırma oparaotleri           mantıksal oparatörler
+
+          + toplama
+         - çıkarma
+         * çarpma
+         / bölme
+         ++
+         --
+          */
+
+         int a=7,c=3,d;
+         int b=3;
+
+         int sonuc;
+         double bolmeSonucu,x=7,y=3; //ondalıklı bolme sonucu icin
+        //Degisken isimleri farklı olmalıdır
+
+         sonuc=a+b;
+        System.out.println("toplama ıslemi: "+sonuc);
+
+        sonuc=a-b;
+        System.out.println("cıkarma islemi: "+sonuc);
+
+        sonuc=a*b;
+        System.out.println("carpma islemi: "+sonuc);
+
+        sonuc=a/b;
+        System.out.println("bolme islemi: "+sonuc);
+
+        bolmeSonucu=x/y;
+        System.out.println("double bolme sonucu: "+bolmeSonucu);
+
+        sonuc=a%b;
+        System.out.println("(Mod Alma) sayının kalanını bulma ıslemı: "+sonuc);
+
+
+
 
 
     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
