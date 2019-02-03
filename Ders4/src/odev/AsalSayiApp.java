d6e7f2c58bb6358b814746b064b890948c1e05dc
ufuk.paksoy <ufuk.paksoy@icloud.com>
2019-02-03 14:16:18 +0300

    Kosullu ifadeler

---
+++ Ders4/src/odev/AsalSayiApp.java
@@ -0,0 +1,17 @@
+package odev;
+
+/*
+Klavyeden girilen sayının
+asal olup olmadığını bulan program
+ */
+public class AsalSayiApp {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+}
